1fd2ae71b3851355678d275c136ac3393cb2fa9c
Anabol1ks <grigorogannisyan.12@gmail.com>
2025-10-28 09:52:30 +0300

    выгрузка из актуального датасета

---
+++ realval/sql/export_listings_sale.sql
@@ -0,0 +1,10 @@
+SELECT
+  deal_type, city, district,
+  price_rub, price_period, price_per_m2,
+  rooms, area_total, area_living, area_kitchen,
+  floor, floors_total, year_built, house_material, condition,
+  lat, lon, dist_to_metro_m, dist_to_center_km, density_500m,
+  metro_station, metro_walk_min, first_seen, last_seen
+FROM listing_master
+WHERE is_active
+  AND deal_type = 'sale'
